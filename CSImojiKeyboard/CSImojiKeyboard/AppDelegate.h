6916125d6a9cab9a97f676fcdcb80d1032523423
SuhyongChoi <chsy0823@gmail.com>
2016-07-20 15:16:24 +0900

    first commit

---
+++ CSImojiKeyboard/CSImojiKeyboard/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CSImojiKeyboard
+//
+//  Created by SuhyongChoi on 2016. 7. 20..
+//  Copyright © 2016년 Elenore. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CSImojiKeyboard/CSImojiKeyboard/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CSImojiKeyboard
+//
+//  Created by SuhyongChoi on 2016. 7. 20..
+//  Copyright © 2016년 Elenore. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
